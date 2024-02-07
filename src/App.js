2390b05170038681c7cebb351c724d2383314297
user <mokapoop24@gmail.com>
2024-02-07 21:33:34 +0200

    clock fix & update - v2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header ">
-        <CoolClock widthValue={25} widthParam="vmin" />
+        <CoolClock widthValue={40} widthParam="vmax" />
       </header>
     </div>
   );
